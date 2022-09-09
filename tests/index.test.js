c0587b90be0afd4d92fad156d41f87c850e6a515
Bernat <bernatpavon@gmail.com>
2022-09-09 09:15:33 +0200

    add: response object length test

--- tests/index.test.js
+++ tests/index.test.js
@@ -19,6 +19,10 @@ test('GET /2 there are 60 articles', async () => {
   const response = await api.get('/2');
   expect(response.body).toHaveLength(60);
 });
+test('GET / each object should have 8 keys', async () => {
+  const response = await api.get('/');
+  expect(Object.keys(response.body[0]).length).toBe(8);
+});
 
 afterAll(() => {
   server.close();
